02909891c2afc77515a10047e2fd793f0338a938
alex <90472869+alexscott06@users.noreply.github.com>
2024-02-06 10:03:03 -0800

    v0.1

---
+++ testproject2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace testproject2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int myInt = 0;
+        float myFloat = 3.14;
+        bool myBool = true;
+        string myString = "The FSCJ trip was boring.\n";
+
+        Console.WriteLine("My Integer variable is " + myInt);
+        Console.WriteLine("My Float variable is " + myFloat);
+        Console.WriteLine("My Boolean variable is " + myBool);
+        Console.WriteLine("My String variable is " + myString);
+
+        for (int i = 0; i <= 20; i++) {
+            Console.WriteLine("I ate " + i + " slices of pizza today.\n");
+
+        }
+
+    }
+}
